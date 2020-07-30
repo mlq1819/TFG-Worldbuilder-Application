1a51045c3b8c54158441825bb314f0541bb6118d
Michael Quinn <mquinn5@binghamton.edu>
2020-07-30 17:28:37 -0400

    working on creating a custom control for borderlevels

---
+++ TFG Worldbuilder Application/MyBorderLevel.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace TFG_Worldbuilder_Application
+{
+    public sealed partial class MyBorderLevel : UserControl
+    {
+        public BorderLevel Level
+        {
+            get
+            {
+                return (BorderLevel)GetValue(levelproperty);
+            }
+            set
+            {
+                SetValue(levelproperty, value);
+            }
+        }
+
+        public uint StrokeThickness
+        {
+            get
+            {
+                return (uint)GetValue(strokethicknessproperty);
+            }
+            set
+            {
+                SetValue(strokethicknessproperty, value);
+            }
+        }
+
+        public string Stroke
+        {
+            get
+            {
+                return (string)GetValue(strokeproperty);
+            }
+            set
+            {
+                SetValue(strokeproperty, value);
+            }
+        }
+
+        public MyBorderLevel()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty levelproperty = DependencyProperty.Register("Level", typeof(BorderLevel), typeof(MyBorderLevel), new PropertyMetadata(0));
+        public static readonly DependencyProperty strokethicknessproperty = DependencyProperty.Register("StrokeThickness", typeof(uint), typeof(MyBorderLevel), new PropertyMetadata(0));
+        public static readonly DependencyProperty strokeproperty = DependencyProperty.Register("Stroke", typeof(string), typeof(MyBorderLevel), new PropertyMetadata(0));
+    }
+}
