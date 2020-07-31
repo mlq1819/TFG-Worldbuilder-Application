893b2bc50eeb1772a8966f9dc18ec6daad068122
Michael Quinn <mquinn5@binghamton.edu>
2020-07-31 16:36:00 -0400

    wrote main code for ViewMenuControl

--- TFG Worldbuilder Application/ViewMenuControl.xaml.cs
+++ TFG Worldbuilder Application/ViewMenuControl.xaml.cs
@@ -19,15 +19,71 @@ namespace TFG_Worldbuilder_Application
 {
     public sealed partial class ViewMenuControl : UserControl
     {
-        public ActiveContext Context
+        public Level2Subtypes Level2
         {
             get
             {
-                return (ActiveContext)GetValue(contextproperty);
+                return (Level2Subtypes)GetValue(level2property);
             }
             set
             {
-                SetValue(contextproperty, (ActiveContext)value);
+                SetValue(level2property, (Level2Subtypes)value);
+            }
+        }
+        public Level3Subtypes Level3
+        {
+            get
+            {
+                return (Level3Subtypes)GetValue(level3property);
+            }
+            set
+            {
+                SetValue(level3property, (Level3Subtypes)value);
+            }
+        }
+        public Level4Subtypes Level4
+        {
+            get
+            {
+                return (Level4Subtypes)GetValue(level4property);
+            }
+            set
+            {
+                SetValue(level4property, (Level4Subtypes)value);
+            }
+        }
+        public Level5Subtypes Level5
+        {
+            get
+            {
+                return (Level5Subtypes)GetValue(level5property);
+            }
+            set
+            {
+                SetValue(level5property, (Level5Subtypes)value);
+            }
+        }
+        public Level6Subtypes Level6
+        {
+            get
+            {
+                return (Level6Subtypes)GetValue(level6property);
+            }
+            set
+            {
+                SetValue(level6property, (Level6Subtypes)value);
+            }
+        }
+
+        public string SubMenuName
+        {
+            get
+            {
+                return (string)GetValue(submenunameproperty);
+            }
+            set
+            {
+                SetValue(submenunameproperty, (string)value);
             }
         }
 
@@ -36,6 +92,11 @@ namespace TFG_Worldbuilder_Application
             this.InitializeComponent();
         }
 
-        public static readonly DependencyProperty contextproperty = DependencyProperty.Register("Context", typeof(ActiveContext), typeof(ViewMenuControl), new PropertyMetadata(0));
+        public static readonly DependencyProperty level2property = DependencyProperty.Register("Level2", typeof(Level2Subtypes), typeof(ViewMenuControl), new PropertyMetadata(0));
+        public static readonly DependencyProperty level3property = DependencyProperty.Register("Level3", typeof(Level3Subtypes), typeof(ViewMenuControl), new PropertyMetadata(0));
+        public static readonly DependencyProperty level4property = DependencyProperty.Register("Level4", typeof(Level4Subtypes), typeof(ViewMenuControl), new PropertyMetadata(0));
+        public static readonly DependencyProperty level5property = DependencyProperty.Register("Level5", typeof(Level5Subtypes), typeof(ViewMenuControl), new PropertyMetadata(0));
+        public static readonly DependencyProperty level6property = DependencyProperty.Register("Level6", typeof(Level6Subtypes), typeof(ViewMenuControl), new PropertyMetadata(0));
+        public static readonly DependencyProperty submenunameproperty = DependencyProperty.Register("SubMenuName", typeof(string), typeof(ViewMenuControl), new PropertyMetadata(0));
     }
 }
